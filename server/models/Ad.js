fdb056b46022b41d5785c103503d418d700ac9c3
Wissler <Joshua.Wissler@malteser.org>
2025-06-06 09:35:05 +0200

    Feature Create-Ad komplett fertig

--- server/models/Ad.js
+++ server/models/Ad.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const adSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: String,
   description: String,
   price: Number,
-  image: String, // später optional für Bild-URL oder Dateinamen
-  createdAt: { type: Date, default: Date.now }
-});
+  image: String,
+  category: String,
+  zipCode: String,
+  location: String,
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  }, // <-- Wichtig
+}, { timestamps: true });
 
 module.exports = mongoose.model('Ad', adSchema);
